35d959c85a31a2b8b06487c88ef6db9e20a7c270
Ripal Nathuji <rnathuji@neoteklabs.com>
2021-01-27 15:48:49 -0600

    Add language server setup / launch to client

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,10 +2,18 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
 
 const resourceRootDir = path.join(__dirname, '../src/') // because the extension is running in the ./out/ subdir
+let client: LanguageClient;
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+	launchLanguageServer(context);
 	showDashboard(context)
 	showImageUpload(context)
 
@@ -86,6 +94,51 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 }
 
 export async function deactivate(): Promise<void> {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
+
+function launchLanguageServer(context: vscode.ExtensionContext) {
+	let serverModule = context.asAbsolutePath(
+		path.join('server', 'dist', 'server.js')
+	);
+	// The debug options for the server
+	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+			options: debugOptions
+		}
+	};
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'xml' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerCnxml',
+		'CNXML Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
 }
 
 function showImageUpload(context: vscode.ExtensionContext) {
