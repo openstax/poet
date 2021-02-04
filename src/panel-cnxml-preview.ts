883d7a07dc01c2495f83ed3cb86e06ba01688cdd
tomjw64 <tom.jw64@gmail.com>
2021-02-04 13:19:47 -0600

    Clean up panel-cnxml-preview

--- src/panel-cnxml-preview.ts
+++ src/panel-cnxml-preview.ts
@@ -3,78 +3,78 @@ import fs from 'fs';
 import path from 'path';
 import { fixResourceReferences, fixCspSourceReferences, addBaseHref, getLocalResourceRoots } from './utils';
 
-export function showCnxmlPreview(resourceRootDir: string) {
-  return async (uri?: vscode.Uri, previewSettings?: any) => {
-    let resource = uri;
-    let contents: string | null = null;
-    if (vscode.window.activeTextEditor && vscode.window.activeTextEditor.document.uri === uri) {
-      contents = vscode.window.activeTextEditor.document.getText();
+export const showCnxmlPreview = (resourceRootDir: string) => async (uri?: vscode.Uri, previewSettings?: any) => {
+  let resource = uri;
+  let contents: string | null = null;
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    const activeDocument = editor.document;
+    if (activeDocument.uri === uri) {
+      contents = activeDocument.getText();
     }
     // support previewing XML that has not been saved yet
     if (!(resource instanceof vscode.Uri)) {
-      if (vscode.window.activeTextEditor) {
-        resource = vscode.window.activeTextEditor.document.uri;
-        contents = vscode.window.activeTextEditor.document.getText();
-      }
+      resource = activeDocument.uri;
+      contents = activeDocument.getText();
     }
-    if (!resource) { return; }
+  }
+  if (!resource) { return; }
 
-    if (!contents) {
-      contents = fs.readFileSync(resource.fsPath, 'utf-8');
+  if (!contents) {
+    contents = fs.readFileSync(resource.fsPath, 'utf-8');
+  }
+
+  const resourceColumn = (vscode.window.activeTextEditor && vscode.window.activeTextEditor.viewColumn) || vscode.ViewColumn.One;
+  const previewColumn = resourceColumn + 1; // because the preview is on the side
+
+  const panel = vscode.window.createWebviewPanel(
+    'openstax.cnxmlPreview',
+    `Preview ${path.basename(resource.fsPath)}`,
+    previewColumn, {
+      enableScripts: true,
+      localResourceRoots: getLocalResourceRoots([vscode.Uri.file(resourceRootDir),], resource),
+      enableFindWidget: true, });
+
+  let html = fs.readFileSync(path.join(resourceRootDir, 'cnxml-preview.html'), 'utf-8');
+  html = addBaseHref(panel.webview, resource, html);
+  html = fixResourceReferences(panel.webview, html, resourceRootDir);
+  html = fixCspSourceReferences(panel.webview, html);
+  panel.webview.html = html;
+
+  const xml = contents;
+  panel.webview.postMessage({xml});
+  let throttleTimer = setTimeout(updatePreview, 200);
+
+  async function updatePreview() {
+    clearTimeout(throttleTimer);
+    let document: vscode.TextDocument;
+    if (!resource) {return;} // it will never be empty at this point
+    try {
+      document = await vscode.workspace.openTextDocument(resource);
+    } catch {
+      return;
     }
-
-    const resourceColumn = (vscode.window.activeTextEditor && vscode.window.activeTextEditor.viewColumn) || vscode.ViewColumn.One;
-    const previewColumn = resourceColumn + 1; // because the preview is on the side
-
-    const panel = vscode.window.createWebviewPanel(
-      'openstax.cnxmlPreview',
-      `Preview ${path.basename(resource.fsPath)}`,
-      previewColumn, {
-        enableScripts: true,
-        localResourceRoots: getLocalResourceRoots([vscode.Uri.file(resourceRootDir),], resource),
-        enableFindWidget: true, });
-
-    let html = fs.readFileSync(path.join(resourceRootDir, 'cnxml-preview.html'), 'utf-8');
-    html = addBaseHref(panel.webview, resource, html);
-    html = fixResourceReferences(panel.webview, html, resourceRootDir);
-    html = fixCspSourceReferences(panel.webview, html);
-    panel.webview.html = html;
-
-    const xml = contents;
-    panel.webview.postMessage({xml});
-    let throttleTimer = setTimeout(updatePreview, 200);
-
-    async function updatePreview() {
-      clearTimeout(throttleTimer);
-      let document: vscode.TextDocument;
-      if (!resource) {return;} // it will never be empty at this point
-      try {
-        document = await vscode.workspace.openTextDocument(resource);
-      } catch {
-        return;
-      }
-      const newContents = document.getText();
-      if (contents !== newContents) {
-        contents = newContents;
-        panel.webview.postMessage({xml: contents});
-      }
-      throttleTimer = setTimeout(updatePreview, 200);
+    const newContents = document.getText();
+    if (contents !== newContents) {
+      contents = newContents;
+      panel.webview.postMessage({xml: contents});
     }
+    throttleTimer = setTimeout(updatePreview, 200);
+  }
 
-    // https://code.visualstudio.com/api/extension-guides/webview#scripts-and-message-passing
-    panel.webview.onDidReceiveMessage(async (message) => {
-      // Replace the full-source version with what came out of the preview panel
-      if (!resource) { return; } // it will never be empty at this point
-      try {
-        const document = await vscode.workspace.openTextDocument(resource);
-        const fullRange = new vscode.Range(
-          document.positionAt(0),
-          document.positionAt(document.getText().length)
-        );
-        const edit = new vscode.WorkspaceEdit();
-        edit.replace(resource, fullRange, message.xml);
-        vscode.workspace.applyEdit(edit);
-      } catch { }
-    });
-  };
+  // https://code.visualstudio.com/api/extension-guides/webview#scripts-and-message-passing
+  panel.webview.onDidReceiveMessage(async (message) => {
+    // Replace the full-source version with what came out of the preview panel
+    if (!resource) { return; } // it will never be empty at this point
+    try {
+      const document = await vscode.workspace.openTextDocument(resource);
+      const fullRange = new vscode.Range(
+        document.positionAt(0),
+        document.positionAt(document.getText().length)
+      );
+      const edit = new vscode.WorkspaceEdit();
+      edit.replace(resource, fullRange, message.xml);
+      vscode.workspace.applyEdit(edit);
+    } catch { }
+  });
 }
