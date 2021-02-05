6d42b84546c67abd1c139289d6fe1ae04351fb52
tomjw64 <tom.jw64@gmail.com>
2021-02-05 14:20:01 -0600

    Use standard for linting

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
-import vscode from 'vscode';
-import path from 'path';
-import { showTocEditor } from './panel-toc-editor';
-import { showImageUpload } from './panel-image-upload';
-import { showCnxmlPreview } from './panel-cnxml-preview';
+import vscode from 'vscode'
+import path from 'path'
+import { showTocEditor } from './panel-toc-editor'
+import { showImageUpload } from './panel-image-upload'
+import { showCnxmlPreview } from './panel-cnxml-preview'
 
-const resourceRootDir = path.join(__dirname); // extension is running in dist/
+const resourceRootDir = path.join(__dirname) // extension is running in dist/
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
-	vscode.commands.registerCommand('openstax.showTocEditor', showTocEditor(resourceRootDir));
-	vscode.commands.registerCommand('openstax.showImageUpload', showImageUpload(resourceRootDir));
-	vscode.commands.registerCommand('openstax.showPreviewToSide', showCnxmlPreview(resourceRootDir));
+  vscode.commands.registerCommand('openstax.showTocEditor', showTocEditor(resourceRootDir))
+  vscode.commands.registerCommand('openstax.showImageUpload', showImageUpload(resourceRootDir))
+  vscode.commands.registerCommand('openstax.showPreviewToSide', showCnxmlPreview(resourceRootDir))
 }
 
 export async function deactivate(): Promise<void> {
--- src/panel-image-upload.ts
+++ src/panel-image-upload.ts
@@ -1,46 +1,46 @@
-import vscode from 'vscode';
-import fs from 'fs';
-import path from 'path';
-import { fixResourceReferences, fixCspSourceReferences, getRootPathUri } from './utils';
+import vscode from 'vscode'
+import fs from 'fs'
+import path from 'path'
+import { fixResourceReferences, fixCspSourceReferences, getRootPathUri } from './utils'
 
 export const showImageUpload = (resourceRootDir: string) => async () => {
   const panel = vscode.window.createWebviewPanel(
-    "openstax.imageUpload",
-    "ImageUpload",
+    'openstax.imageUpload',
+    'ImageUpload',
     vscode.ViewColumn.One,
     {
-      enableScripts: true,
-    },
-  );
+      enableScripts: true
+    }
+  )
 
-  let html = fs.readFileSync(path.join(resourceRootDir, 'image-upload.html'), 'utf-8');
-  html = fixResourceReferences(panel.webview, html, resourceRootDir);
-  html = fixCspSourceReferences(panel.webview, html);
-  panel.webview.html = html;
+  let html = fs.readFileSync(path.join(resourceRootDir, 'image-upload.html'), 'utf-8')
+  html = fixResourceReferences(panel.webview, html, resourceRootDir)
+  html = fixCspSourceReferences(panel.webview, html)
+  panel.webview.html = html
 
-  panel.reveal(vscode.ViewColumn.One);
+  panel.reveal(vscode.ViewColumn.One)
 
   panel.webview.onDidReceiveMessage(async (message) => {
-    const { mediaUploads } = message;
-    const uri = getRootPathUri();
+    const { mediaUploads } = message
+    const uri = getRootPathUri()
     if (mediaUploads == null || uri == null) {
-      return;
+      return
     }
     for (const upload of mediaUploads) {
-      const { mediaName, data } = upload;
+      const { mediaName, data } = upload
       // vscode.Uri.joinPath is not in the latest theia yet
       // const newFileUri = vscode.Uri.joinPath(uri, 'media', mediaName);
-      const newFileUri = uri.with({ path: path.join(uri.path, 'media', mediaName) });
+      const newFileUri = uri.with({ path: path.join(uri.path, 'media', mediaName) })
       try {
-        await vscode.workspace.fs.stat(newFileUri);
+        await vscode.workspace.fs.stat(newFileUri)
         // File exists already, do nothing for now
       } catch (err) {
         if (err instanceof vscode.FileSystemError && err.name.includes('EntryNotFound')) {
-          console.log(`writing: ${newFileUri.toString()}`);
-          const content = Buffer.from(data.split(',')[1], 'base64');
-          vscode.workspace.fs.writeFile(newFileUri, content);
+          console.log(`writing: ${newFileUri.toString()}`)
+          const content = Buffer.from(data.split(',')[1], 'base64')
+          await vscode.workspace.fs.writeFile(newFileUri, content)
         }
       }
     }
-  });
-};
+  })
+}
