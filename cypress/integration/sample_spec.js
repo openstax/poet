880c69bce99c575957597e0d525055219268057c
philschatz <253202+philschatz@users.noreply.github.com>
2021-02-12 14:33:05 -0600

    provide a stub for acquireVsCodeApi

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,7 +3,12 @@ import path from 'path'
 
 describe('My First Test', () => {
     it('Does not do much!', () => {
-      cy.visit('./dist/cnxml-preview.html')
+      cy.visit('./dist/cnxml-preview.html', {
+        onBeforeLoad: (contentWindow) => {
+          contentWindow.acquireVsCodeApi = () => { return 'HOORAY I AM NOT UNDEFINED!' }
+        }
+      })
+
       expect(true).to.equal(true)
     })
   })
