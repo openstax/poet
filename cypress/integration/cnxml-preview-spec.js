144cf22a84510073842520d561a015e358bb3869
philschatz <253202+philschatz@users.noreply.github.com>
2021-02-18 12:37:01 -0600

    :art: clarify the helper methods
    and how they work

--- cypress/integration/cnxml-preview-spec.js
+++ cypress/integration/cnxml-preview-spec.js
@@ -4,22 +4,24 @@
 const htmlPath = './out/cnxml-preview.html'
 
 describe('cnxml-preview Webview Tests', () => {
-  function postMessage(msg) {
+  function sendMessage(msg) {
     cy.window().then($window => {
       $window.postMessage(msg, '*')
     })
   }
-  function postXml(xmlStr) {
-    postMessage({xml: xmlStr})
+  function sendXml(xmlStr) {
+    sendMessage({xml: xmlStr})
   }
 
-  let pending = []
+  // When the browser calls vscode.postMessage(...) that message is added to this array
+  let messagesFromWidget = []
 
+  // Load the HTML file and inject the acquireVsCodeApi() stub.
   beforeEach(() => {
     cy.visit(htmlPath, {
       onBeforeLoad: (contentWindow) => {
         class API {
-          postMessage(msg) { pending.push(msg) }
+          postMessage(msg) { messagesFromWidget.push(msg) }
         }
         contentWindow.acquireVsCodeApi = () => { return new API() }
       }
@@ -27,68 +29,68 @@ describe('cnxml-preview Webview Tests', () => {
   })
 
   it('Errors when something other than an object with an xml field is sent', () => {
-    postMessage({somethingOtherThanXml: 'hello'})
+    sendMessage({somethingOtherThanXml: 'hello'})
     cy.get('#preview *').should('not.exist')
   })
 
   it('Errors when malformed XML is sent', () => {
-    postXml('<invalid-xml-element-name')
+    sendXml('<invalid-xml-element-name')
     cy.get('#preview parsererror').should('exist')
   })
 
   it('Does not error when valid XML is ssent', () => {
-    postXml('<valid-xml-element/>')
+    sendXml('<valid-xml-element/>')
     cy.get('#preview parsererror').should('not.exist')
   })
 
   it('Updates the DOM when new content is sent', () => {
-    postXml('<document><para>I am a paragraph</para></document>')
+    sendXml('<document><para>I am a paragraph</para></document>')
     cy.get('#preview p').should('exist')
     cy.get('#preview ul').should('not.exist')
-    postXml('<document><list><item>I am a list with one item</item></list></document>')
+    sendXml('<document><list><item>I am a list with one item</item></list></document>')
     cy.get('#preview p').should('not.exist')
     cy.get('#preview ul').should('exist')
   })
 
   it('Sends XML back when clicked (remove this. it should send a command instead of the whole CNXML)', () => {
-    postXml('<howdy/>')
+    sendXml('<howdy/>')
     cy.get('#advancedDetails').click() // expand
     cy.get('#sendButton').click().then(() => {
-      expect(pending.length).equal(1)
-      expect(pending[0].xml).equal('<howdy/>')
+      expect(messagesFromWidget.length).equal(1)
+      expect(messagesFromWidget[0].xml).equal('<howdy/>')
     })
   })
 
   describe('cnxml->html conversion', () => {
     it('Translates CNXML tags to HTML', () => {
-      postXml('<para>I am a paragraph</para>')
+      sendXml('<para>I am a paragraph</para>')
       cy.get('#preview para').should('not.exist')
       cy.get('#preview p').should('exist')
     })
     
     it('Ignores comments', () => {
-      postXml('<document><!-- I am a comment --></document>')
+      sendXml('<document><!-- I am a comment --></document>')
       cy.get('#preview document').should('exist')
     })
   
     it('Removes some elements like metadata', () => {
-      postXml('<document><metadata><para>Para in a metadata</para></metadata></document>')
+      sendXml('<document><metadata><para>Para in a metadata</para></metadata></document>')
       cy.get('#preview p').should('not.exist')
     })
   })
 
   describe('Handling math', () => {
     it('Strips the m: prefix from math elements', () => {
-      postXml('<document><m:math xmlns:m="https://openstax.org/anything"><m:mtext>I am a math element</m:mtext></m:math></document>')
+      sendXml('<document><m:math xmlns:m="https://openstax.org/anything"><m:mtext>I am a math element</m:mtext></m:math></document>')
       cy.get('#preview math').should('exist')
     })
   
     it('Updates Math nodes in the DOM when new content is sent', () => {
       // MathJax requires some hacky work
-      postXml('<document><math><mi>x</mi></math></document>')
+      sendXml('<document><math><mi>x</mi></math></document>')
       cy.get('#preview mi').should('exist')
       cy.get('#preview mn').should('not.exist')
-      postXml('<document><math><mn>2</mn></math></document>')
+      sendXml('<document><math><mn>2</mn></math></document>')
       cy.get('#preview mi').should('not.exist')
       cy.get('#preview mn').should('exist')
     })
@@ -96,13 +98,13 @@ describe('cnxml-preview Webview Tests', () => {
 
   describe('VirtualDOM (vdom)', () => {
     it('removes an element', () => {
-      postXml('<root><child/></root>')
-      postXml('<root/>')
+      sendXml('<root><child/></root>')
+      sendXml('<root/>')
     })
 
     it('removes an attribute', () => {
-      postXml('<root id="id123"/>')
-      postXml('<root/>')
+      sendXml('<root id="id123"/>')
+      sendXml('<root/>')
     })
   })
 
