9da054dab91a48db2074ba7e275bc62ec33fbac3
philschatz <253202+philschatz@users.noreply.github.com>
2021-02-23 11:23:29 -0600

    add server test coverage

--- scripts/pretest.sh
+++ scripts/pretest.sh
@@ -13,3 +13,4 @@ npm run webpack
 cp -r ./client/dist/* ./client/out/
 cp -r ./client/src/test/data/ ./client/out/test/data/
 nyc instrument --compact=false --source-map --in-place ./client/out/ ./client/out/
+# nyc instrument --compact=false --source-map --in-place ./server/out/ ./server/out/
