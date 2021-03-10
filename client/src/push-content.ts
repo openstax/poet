c55a09dcdd045098bcf74ad6f6b2b94eb876a1aa
Chris Kline <ckline.tryptic@gmail.com>
2021-03-10 20:10:52 +0000

    eslint fixes

--- client/src/push-content.ts
+++ client/src/push-content.ts
@@ -7,36 +7,34 @@ export const pushContent = (resourceRootDir: string) => async () => {
   const api = gitExtension.getAPI(1)
   const repo = api.repositories[0]
 
-  const commitOptions: CommitOptions = {
-    all: true
-  }
+  const commitOptions: CommitOptions = { all: true }
 
   let commitSucceeded = false
+
   try {
     await repo.commit('poet commit', commitOptions)
     commitSucceeded = true
   } catch (e) {
     if (e.stdout == null) { throw e }
-    void vscode.window.showErrorMessage(e.gitErrorCode)
     if ((e.stdout as string).includes('nothing to commit')) {
       void vscode.window.showErrorMessage('No changes to push.')
     } else {
-      const message = e.gitErrorCode ? e.gitErrorCode : e.message
-      void vscode.window.showErrorMessage(`Push failed: ${e.message}`)
+      const message: string = e.gitErrorCode === undefined ? e.message : e.gitErrorCode
+      void vscode.window.showErrorMessage(`Push failed: ${message}`)
     }
   }
+
   if (commitSucceeded) {
     try {
       await repo.pull()
       await repo.push()
       void vscode.window.showInformationMessage('Successful content push.')
     } catch (e) {
-      console.log(e)
       if (e.gitErrorCode == null) { throw e }
       if (e.gitErrorCode === GitErrorCodes.Conflict) {
         void vscode.window.showErrorMessage('Content conflict, please resolve.')
       } else {
-        void vscode.window.showErrorMessage(`Push failed: ${e.message}`)
+        void vscode.window.showErrorMessage(`Push failed: ${e.message as string}`)
       }
     }
   }
