4f3ff890b080cc84a0b3fec49c0f44d0a1e648a6
Philip Schatz <253202+philschatz@users.noreply.github.com>
2021-03-15 21:40:14 -0500

    Detect Theia via environment vars

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -72,8 +72,10 @@ export function getRootPathUri(): vscode.Uri | null {
  * Asserts a value of a nullable type is not null and returns the same value with a non-nullable type
  */
 export function expect<T>(value: T | null | undefined, message?: string): T {
+  message = message ?? 'Unwrapped a null value'
   if (value == null) {
-    throw new Error(message ?? 'Unwrapped a null value')
+    vscode.window.showErrorMessage(message)
+    throw new Error(message)
   }
   return value
 }
