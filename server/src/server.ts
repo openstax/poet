95ac105675b4870e22ce7a775cad84aa62fcd61c
Brenda Nguyen <bn16@rice.edu>
2021-05-03 08:58:52 -0500

    make server requests stubbable for testing

--- server/src/server.ts
+++ server/src/server.ts
@@ -156,45 +156,54 @@ connection.onRequest('onDidChangeWorkspaceFolders', async (event) => {
   }
 })
 
-connection.onRequest(ExtensionServerRequest.BundleTrees, async ({ workspaceUri }: BundleTreesArgs): Promise<BundleTreesResponse> => {
-  const bundleAndValidator = workspaceBookBundles.get(workspaceUri)
-  if (bundleAndValidator == null) { return null }
-  const bundle = bundleAndValidator[0]
-  const promises = bundle.collectionItems().map(async (collection: BundleItem): Promise<TocTreeCollection[]> => {
-    try {
-      const tree = expect(await bundle.collectionTree(collection.key), 'collection must exist').inner
-      return [tree]
-    } catch {
-      const uri = expect(bundle.bundleItemToUri(collection), 'No root path to generate diagnostic')
-      const diagnostics = expect(await collectionDiagnostic(), 'No diagnostic to generate')
-      connection.sendDiagnostics({
-        uri,
-        diagnostics
-      })
-      return []
-    }
-  })
-  const trees = (await Promise.all(promises)).flat()
-  return trees
-})
+export function bundleTreesHandler(workspaceBookBundles: Map<string, [BookBundle, BundleValidationQueue]>): (request: BundleTreesArgs) => Promise<BundleTreesResponse> {
+  return async (request: BundleTreesArgs) => {
+    const bundleAndValidator = workspaceBookBundles.get(request.workspaceUri)
+    if (bundleAndValidator == null) { return null }
+    const bundle = bundleAndValidator[0]
+    const promises = bundle.collectionItems().map(async (collection: BundleItem): Promise<TocTreeCollection[]> => {
+      try {
+        const tree = expect(await bundle.collectionTree(collection.key), 'collection must exist').inner
+        return [tree]
+      } catch {
+        const uri = expect(bundle.bundleItemToUri(collection), 'No root path to generate diagnostic')
+        const diagnostics = expect(await collectionDiagnostic(), 'No diagnostic to generate')
+        connection.sendDiagnostics({
+          uri,
+          diagnostics
+        })
+        return []
+      }
+    })
+    const trees = (await Promise.all(promises)).flat()
+    return trees
+  }
+}
+connection.onRequest(ExtensionServerRequest.BundleTrees, bundleTreesHandler(workspaceBookBundles))
 
-connection.onRequest(ExtensionServerRequest.BundleOrphanedModules, async ({ workspaceUri }: BundleOrphanedModulesArgs): Promise<BundleOrphanedModulesResponse> => {
-  const bundleAndValidator = workspaceBookBundles.get(workspaceUri)
-  if (bundleAndValidator == null) { return null }
-  const bundle = bundleAndValidator[0]
-  const orphanModules = Array.from((await bundle.orphanedModules()).inner)
-  const result = await Promise.all(orphanModules.map(async m => await bundle.moduleAsTreeObject(m)))
-  return result
-})
+export function bundleOrphanedModulesHandler(workspaceBookBundles: Map<string, [BookBundle, BundleValidationQueue]>): (request: BundleOrphanedModulesArgs) => Promise<BundleOrphanedModulesResponse> {
+  return async (request: BundleOrphanedModulesArgs) => {
+    const bundleAndValidator = workspaceBookBundles.get(request.workspaceUri)
+    if (bundleAndValidator == null) { return null }
+    const bundle = bundleAndValidator[0]
+    const orphanModules = Array.from((await bundle.orphanedModules()).inner)
+    const result = await Promise.all(orphanModules.map(async m => await bundle.moduleAsTreeObject(m)))
+    return result
+  }
+}
+connection.onRequest(ExtensionServerRequest.BundleOrphanedModules, bundleOrphanedModulesHandler(workspaceBookBundles))
 
-connection.onRequest(ExtensionServerRequest.BundleModules, async ({ workspaceUri }: BundleModulesArgs): Promise<BundleModulesResponse> => {
-  const bundleAndValidator = workspaceBookBundles.get(workspaceUri)
-  if (bundleAndValidator == null) { return null }
-  const bundle = bundleAndValidator[0]
-  const modules = bundle.modules()
-  const result = await Promise.all(modules.map(async m => await bundle.moduleAsTreeObject(m)))
-  return result
-})
+export function bundleModulesHandler(workspaceBookBundles: Map<string, [BookBundle, BundleValidationQueue]>): (request: BundleModulesArgs) => Promise<BundleModulesResponse> {
+  return async (request: BundleModulesArgs) => {
+    const bundleAndValidator = workspaceBookBundles.get(request.workspaceUri)
+    if (bundleAndValidator == null) { return null }
+    const bundle = bundleAndValidator[0]
+    const modules = bundle.modules()
+    const result = await Promise.all(modules.map(async m => await bundle.moduleAsTreeObject(m)))
+    return result
+  }
+}
+connection.onRequest(ExtensionServerRequest.BundleModules, bundleModulesHandler(workspaceBookBundles))
 
 // Make the text document manager listen on the connection
 // for open, change and close text document events
