8211413fdc6a3dc23d5110f46a111b52d8acec6a
therealmarv <1050582+therealmarv@users.noreply.github.com>
2021-07-13 15:42:23 +0200

    add tests

--- server/src/test/server.test.ts
+++ server/src/test/server.test.ts
@@ -26,10 +26,11 @@ import {
   validateModuleImagePaths,
   validateModuleLinks
 } from '../bundle-validation'
-import { DOMParser } from 'xmldom'
+import { DOMParser, XMLSerializer } from 'xmldom'
 import {
   bundleTreesHandler
 } from '../server-handler'
+import { fixDocument } from '../fix-document-ids'
 
 const DIAGNOSTIC_SOURCE = 'cnxml'
 
@@ -1372,6 +1373,168 @@ describe('BookBundle caching', () => {
       const b = { cacheKey: 'b', resetCacheKey }
       const c = { cacheKey: 'c', resetCacheKey }
       assert.deepStrictEqual(cacheSort([b, c, a]), [a, b, c])
+    })
+  })
+})
+describe('Element ID creation', () => {
+  describe('fixDocument', () => {
+    it('check if ids are created', () => {
+      const simple = `<document id="new" cnxml-version="0.7" module-id="" xmlns="http://cnx.rice.edu/cnxml" class="introduction">
+          <title>Introduction</title>
+          <metadata mdml-version="0.5" xmlns:md="http://cnx.rice.edu/mdml">
+            <md:title>Introduction</md:title>
+          </metadata>
+          <content>
+            <para id="test">Test Introduction</para>
+            <para>no id here</para>
+          </content>
+        </document>`
+      const simpleFixed = `<document id="new" cnxml-version="0.7" module-id="" xmlns="http://cnx.rice.edu/cnxml" class="introduction">
+          <title>Introduction</title>
+          <metadata mdml-version="0.5" xmlns:md="http://cnx.rice.edu/mdml">
+            <md:title>Introduction</md:title>
+          </metadata>
+          <content>
+            <para id="test">Test Introduction</para>
+            <para id="para-00001">no id here</para>
+          </content>
+        </document>`
+      const doc = new DOMParser().parseFromString(simple)
+      fixDocument(doc)
+      const out = new XMLSerializer().serializeToString(doc)
+      assert.strictEqual(out, simpleFixed)
+    })
+    it('check if ids are created in right order', () => {
+      const paraPartialId = `<document id="new" cnxml-version="0.7" module-id="" xmlns="http://cnx.rice.edu/cnxml" class="introduction">
+          <title>Introduction</title>
+          <metadata mdml-version="0.5" xmlns:md="http://cnx.rice.edu/mdml">
+            <md:title>Introduction</md:title>
+          </metadata>
+          <content>
+            <section>
+              <para id="para-00001">Test Introduction</para>
+              <para>no id here</para>
+            </section>
+            <section id="sect-00001">
+              <para id="para-00002">oh we have id2 here</para>
+              <para id="para-00003">oh we have id3 here</para>
+              <para>no id here</para>
+              <para>no id here</para>
+            </section>
+          </content>
+        </document>`
+      const doc = new DOMParser().parseFromString(paraPartialId)
+      fixDocument(doc)
+      const NS_CNXML = 'http://cnx.rice.edu/cnxml'
+      const select = xpath.useNamespaces({ cnxml: NS_CNXML })
+      const fixParaNodes = select('//cnxml:para', doc) as Element[]
+      assert.strictEqual(fixParaNodes[0].getAttribute('id'), 'para-00001')
+      assert.strictEqual(fixParaNodes[1].getAttribute('id'), 'para-00004')
+      assert.strictEqual(fixParaNodes[2].getAttribute('id'), 'para-00002')
+      assert.strictEqual(fixParaNodes[3].getAttribute('id'), 'para-00003')
+      assert.strictEqual(fixParaNodes[4].getAttribute('id'), 'para-00005')
+      assert.strictEqual(fixParaNodes[5].getAttribute('id'), 'para-00006')
+      const fixSectionNodes = select('//cnxml:section', doc) as Element[]
+      assert.strictEqual(fixSectionNodes[0].getAttribute('id'), 'sect-00002')
+      assert.strictEqual(fixSectionNodes[1].getAttribute('id'), 'sect-00001')
+    })
+    it('check if ids are generated with right (short) prefix', () => {
+      const xml = `<document id="new" cnxml-version="0.7" module-id="" xmlns="http://cnx.rice.edu/cnxml" class="introduction">
+          <title>Introduction</title>
+          <metadata mdml-version="0.5" xmlns:md="http://cnx.rice.edu/mdml">
+            <md:title>Introduction</md:title>
+          </metadata>
+          <content>
+            <para>no id here</para>
+            <equation/>
+            <list/>
+            <section/>
+            <problem/>
+            <solution/>
+            <exercise/>
+            <example/>
+            <figure/>
+            <definition/>
+            <para>
+              <term>hello</term>
+            </para>
+            <table/>
+            <quote/>
+            <note/>
+            <footnote/>
+            <cite/>
+          </content>
+        </document>`
+      const xmlFixed = `<document id="new" cnxml-version="0.7" module-id="" xmlns="http://cnx.rice.edu/cnxml" class="introduction">
+          <title>Introduction</title>
+          <metadata mdml-version="0.5" xmlns:md="http://cnx.rice.edu/mdml">
+            <md:title>Introduction</md:title>
+          </metadata>
+          <content>
+            <para id="para-00001">no id here</para>
+            <equation id="eq-00001"/>
+            <list id="list-00001"/>
+            <section id="sect-00001"/>
+            <problem id="prob-00001"/>
+            <solution id="sol-00001"/>
+            <exercise id="exer-00001"/>
+            <example id="exam-00001"/>
+            <figure id="fig-00001"/>
+            <definition id="def-00001"/>
+            <para id="para-00002">
+              <term id="term-00001">hello</term>
+            </para>
+            <table id="table-00001"/>
+            <quote id="quote-00001"/>
+            <note id="note-00001"/>
+            <footnote id="foot-00001"/>
+            <cite id="cite-00001"/>
+          </content>
+        </document>`
+      const doc = new DOMParser().parseFromString(xml)
+      fixDocument(doc)
+      const out = new XMLSerializer().serializeToString(doc)
+      assert.strictEqual(out, xmlFixed)
+    })
+    it('check if term in defintion does not get an id', () => {
+      const xml = `<document id="new" cnxml-version="0.7" module-id="" xmlns="http://cnx.rice.edu/cnxml" class="introduction">
+          <title>Introduction</title>
+          <metadata mdml-version="0.5" xmlns:md="http://cnx.rice.edu/mdml">
+            <md:title>Introduction</md:title>
+          </metadata>
+          <content>
+            <para>
+              <term id="term-00001">hello</term>
+            </para>
+            <definition>
+              <term>I should not get an id</term>
+            </definition>
+            <para>
+              <term>need id</term>
+            </para>
+          </content>
+        </document>`
+      const xmlFixed = `<document id="new" cnxml-version="0.7" module-id="" xmlns="http://cnx.rice.edu/cnxml" class="introduction">
+          <title>Introduction</title>
+          <metadata mdml-version="0.5" xmlns:md="http://cnx.rice.edu/mdml">
+            <md:title>Introduction</md:title>
+          </metadata>
+          <content>
+            <para id="para-00001">
+              <term id="term-00001">hello</term>
+            </para>
+            <definition id="def-00001">
+              <term>I should not get an id</term>
+            </definition>
+            <para id="para-00002">
+              <term id="term-00002">need id</term>
+            </para>
+          </content>
+        </document>`
+      const doc = new DOMParser().parseFromString(xml)
+      fixDocument(doc)
+      const out = new XMLSerializer().serializeToString(doc)
+      assert.strictEqual(out, xmlFixed)
     })
   })
 })
