10e6052e908d4155902d7ecd182fea2bfb95f761
philschatz <253202+philschatz@users.noreply.github.com>
2022-05-18 20:06:56 -0500

    :art: refactor so validators can specify message

--- server/src/model/page.ts
+++ server/src/model/page.ts
@@ -1,7 +1,7 @@
 import I from 'immutable'
 import * as Quarx from 'quarx'
-import { Opt, Position, PathKind, WithRange, textWithRange, select, selectOne, calculateElementPositions, expectValue, HasRange, NOWHERE, join, equalsOpt, equalsWithRange, tripleEq, Range } from './utils'
-import { Fileish, ValidationCheck, ValidationKind, ValidationSeverity } from './fileish'
+import { Opt, Position, PathKind, WithRange, textWithRange, select, selectOne, calculateElementPositions, expectValue, HasRange, NOWHERE, join, equalsOpt, equalsWithRange, tripleEq, Range, filterNull } from './utils'
+import { buildValidationCheck, Fileish, ValidationCheck, ValidationKind, ValidationSeverity } from './fileish'
 import { ResourceNode } from './resource'
 
 enum ResourceLinkKind {
@@ -89,16 +89,6 @@ function getContextPagesAndTargets(ex: ExerciseJSON): PagesAndTargets {
 function convertToPos(str: string, cursor: number): Position {
   const lines = str.substring(cursor).split('\n')
   return { line: lines.length, character: lines[lines.length - 1].length }
-}
-
-function filterNull<T>(set: I.Set<Opt<T>>): I.Set<T> {
-  return I.Set<T>().withMutations(s => {
-    set.forEach(s1 => {
-      if (s1 !== undefined) {
-        s.add(s1)
-      }
-    })
-  })
 }
 
 export const UNTITLED_FILE = 'UntitledFile'
@@ -286,97 +276,94 @@ export class PageNode extends Fileish {
     const exerciseCache = this._exerciseCache.get()
     const resourceLinks = this.resourceLinks
     const pageLinks = this.pageLinks
+    const toRange = <T extends HasRange>(l: T) => l.range
+
     return [
-      {
-        message: PageValidationKind.MISSING_RESOURCE,
+      buildValidationCheck({
         nodesToLoad: resourceLinks.map(l => l.target),
-        fn: () => resourceLinks.filter(img => !img.target.exists).map(l => l.range)
-      },
-      {
-        message: PageValidationKind.MISSING_TARGET,
+        itemsToCheck: resourceLinks,
+        toRange,
+        validator: img => img.target.exists ? undefined : PageValidationKind.MISSING_RESOURCE
+      }),
+      buildValidationCheck({
         nodesToLoad: filterNull(pageLinks.map(l => {
           if (l.type !== PageLinkKind.URL && l.type !== PageLinkKind.EXERCISE && l.page !== this) {
             return l.page
           }
           return undefined
         })),
-        fn: () => pageLinks.filter(l => {
-          if (l.type === PageLinkKind.URL) return l.url.startsWith('#') // URL links are ok
-          if (l.type === PageLinkKind.EXERCISE) return false // We check these in a different validation
-          if (!l.page.exists) return true // link to non-existent page are bad
-          if (l.type === PageLinkKind.PAGE) return false // linking to the whole page and it exists is ok
-          return !l.page.hasElementId(l.targetElementId)
-        }).map(l => l.range)
-      },
-      {
-        message: PageValidationKind.MALFORMED_EXERCISE,
+        itemsToCheck: pageLinks,
+        toRange,
+        validator: l => {
+          if (l.type === PageLinkKind.URL) return l.url.startsWith('#') ? PageValidationKind.MISSING_TARGET : undefined // URL links are ok
+          if (l.type === PageLinkKind.EXERCISE) return undefined // We check these in a different validation
+          if (!l.page.exists) return PageValidationKind.MISSING_TARGET // link to non-existent page are bad
+          if (l.type === PageLinkKind.PAGE) return undefined // linking to the whole page and it exists is ok
+          return l.page.hasElementId(l.targetElementId) ? undefined : PageValidationKind.MISSING_TARGET
+        }
+      }),
+      buildValidationCheck({
         nodesToLoad: this.bundle.allPages.all,
-        fn: () => pageLinks.filter(l => {
+        itemsToCheck: pageLinks,
+        toRange,
+        validator: l => {
           if (l.type === PageLinkKind.EXERCISE) {
             const exercises = exerciseCache.get(l.url)
             if (exercises === undefined) {
-              return true // Error: Exercise has not been loaded by now. Could be a bug or an error from server
+              return PageValidationKind.MALFORMED_EXERCISE // Error: Exercise has not been loaded by now. Could be a bug or an error from server
             }
             if (exercises.items.length !== 1) {
-              return true // Error: Expected 1 exercise result but found 0 or at least 2
+              return PageValidationKind.MALFORMED_EXERCISE // Error: Expected 1 exercise result but found 0 or at least 2
             }
             const { pageUUIDs, elementIDs } = getContextPagesAndTargets(exercises.items[0])
             // If there is at least one pageUUID then ensure at least one of them is in our book
             if (pageUUIDs.length > 0) {
               const contextPages = this.bundle.allPages.all.filter(p => pageUUIDs.includes(p.uuid()))
               if (contextPages.size < 1) {
-                return true // Error: Did not find any pages in our bundle for the context for this exercise
+                return PageValidationKind.MALFORMED_EXERCISE // Error: Did not find any pages in our bundle for the context for this exercise
               }
               for (const p of contextPages) {
                 for (const id of elementIDs) {
                   if (!p.elementIds.has(id)) {
-                    return true // Error: context-feature does not exist in the target page 'p'
+                    return PageValidationKind.MALFORMED_EXERCISE // Error: context-feature does not exist in the target page 'p'
                   }
                 }
               }
               if (contextPages.size === 0 || elementIDs.length === 0) {
-                return true // Error: There were no context element IDs
+                return PageValidationKind.MALFORMED_EXERCISE // Error: There were no context element IDs
               }
             } else {
               // Check if the ID in the Exercise matches one on this Page
               const elementIds = Array.from(this.elementIds.keys())
               for (const targetId of elementIDs) {
                 if (!elementIds.includes(targetId)) {
-                  return true // Error: Exercise contains a context element ID but that ID is not available on this Page
+                  return PageValidationKind.MALFORMED_EXERCISE // Error: Exercise contains a context element ID but that ID is not available on this Page
                 }
               }
             }
           }
-          return false
-        }).map(l => l.range)
-      },
-      {
-        message: PageValidationKind.MALFORMED_UUID,
-        nodesToLoad: I.Set(),
-        fn: () => {
-          const uuid = this.ensureLoaded(this._uuid)
-          return UUID_RE.test(uuid.v) ? I.Set() : I.Set([uuid.range])
+          return undefined
         }
-      },
-      {
-        message: PageValidationKind.DUPLICATE_UUID,
+      }),
+      buildValidationCheck({
         nodesToLoad: I.Set(),
-        fn: () => {
-          const uuid = this.ensureLoaded(this._uuid)
-          if (this.bundle.isDuplicateUuid(uuid.v)) {
-            return I.Set([uuid.range])
-          } else {
-            return I.Set()
-          }
-        }
-      },
-      {
-        message: PageValidationKind.MISSING_ID,
+        itemsToCheck: I.Set([this.ensureLoaded(this._uuid)]), // Kinda HACKy since we only check one thing
+        toRange,
+        validator: uuid => UUID_RE.test(uuid.v) ? undefined : PageValidationKind.MALFORMED_UUID
+      }),
+      buildValidationCheck({
         nodesToLoad: I.Set(),
-        fn: () => {
-          return this.ensureLoaded(this._elementsMissingIds)
-        }
-      }
+        itemsToCheck: I.Set([this.ensureLoaded(this._uuid)]),
+        toRange,
+        validator: uuid => this.bundle.isDuplicateUuid(uuid.v) ? PageValidationKind.DUPLICATE_UUID : undefined
+      }),
+      buildValidationCheck({
+        // "Everything that is missing an ID gets a validation error"
+        nodesToLoad: I.Set(),
+        itemsToCheck: I.Set(this.ensureLoaded(this._elementsMissingIds)),
+        toRange: i => i,
+        validator: _ => PageValidationKind.MISSING_ID
+      })
     ]
   }
 }
