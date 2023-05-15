a0f42095aa05366d8ef521c1183c47d16d8a55e2
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-05-15 17:13:26 -0500

    Move readme generator to server

---
+++ server/src/readme-generator.spec.ts
@@ -0,0 +1,109 @@
+import expect from 'expect'
+import I from 'immutable'
+import { BookNode } from './model/book'
+import { bookMaker, bundleMaker, makeBundle } from './model/spec-helpers.spec'
+import { generateReadmeForWorkspace } from './readme-generator'
+
+describe('generate readme', () => {
+  const slugs = [
+    'stuff-things',
+    'stuff-other-things',
+    'other-stuff-other-things'
+  ]
+
+  const titles = [
+    'Stuff and things',
+    'Stuff and other things',
+    'Other stuff and other things'
+  ]
+
+  const initBooks = (books: I.Set<BookNode>) => {
+    books.toArray().forEach((book, idx) => {
+      book.load(bookMaker({ slug: slugs[idx], title: titles[idx] }))
+    })
+  }
+
+  it('generates the readme for a single book', () => {
+    const bundle = makeBundle()
+    bundle.load(bundleMaker({ books: [slugs[0]] }))
+    initBooks(bundle.books)
+    const output = generateReadmeForWorkspace(Array.from(bundle.allBooks.all))
+    const outputLines = output.split('\n')
+    expect(outputLines.length).toBe(14)
+    expect(outputLines[0].startsWith(`# ${titles[0]}`)).toBe(true)
+    expect(outputLines[4].startsWith(`_${titles[0]}_`)).toBe(true)
+    expect(outputLines[6]).toContain(`The book can be viewed [online](https://openstax.org/details/books/${slugs[0]})`)
+  })
+
+  it('generates the readme for a bundle of two', () => {
+    const bundle = makeBundle()
+    bundle.load(bundleMaker({ books: slugs.slice(0, 2) }))
+    initBooks(bundle.books)
+    const output = generateReadmeForWorkspace(Array.from(bundle.allBooks.all))
+    const outputLines = output.split('\n')
+    expect(outputLines.length).toBe(16)
+    expect(outputLines[0].startsWith(`# ${titles[0] + ' and ' + titles[1]}`)).toBe(true)
+    expect(outputLines[4].startsWith(`_${titles[0] + ' and ' + titles[1]}_`)).toBe(true)
+    slugs
+      .slice(0, 2)
+      .map((slug, idx) => [slug, titles[idx]])
+      .forEach(([slug, title], idx: number) => {
+        expect(outputLines[7 + idx]).toContain(`- _${title}_ [online](https://openstax.org/details/books/${slug})`)
+      })
+  })
+
+  it('generates the readme for a bundle of three', () => {
+    const bundle = makeBundle()
+    bundle.load(bundleMaker({ books: slugs }))
+    initBooks(bundle.books)
+    const output = generateReadmeForWorkspace(Array.from(bundle.allBooks.all))
+    const outputLines = output.split('\n')
+    expect(outputLines.length).toBe(17)
+    expect(outputLines[0].startsWith(`# ${titles[0]}, ${titles[1]}, and ${titles[2]}`)).toBe(true)
+    expect(outputLines[4].startsWith(`_${titles[0]}, ${titles[1]}, and ${titles[2]}_`)).toBe(true)
+    slugs
+      .map((slug, idx) => [slug, titles[idx]])
+      .forEach(([slug, title], idx: number) => {
+        expect(outputLines[7 + idx]).toContain(`- _${title}_ [online](https://openstax.org/details/books/${slug})`)
+      })
+  })
+
+  it('fails for bundles that contain various licenses', () => {
+    const bundle = makeBundle()
+    bundle.load(bundleMaker({ books: [slugs[0], slugs[1]] }))
+    bundle.books.toArray().forEach((book, idx) => {
+      book.load(bookMaker({
+        slug: slugs[idx],
+        title: titles[idx],
+        licenseUrl: idx === 0
+          ? 'http://creativecommons.org/licenses/by/4.0'
+          : 'http://creativecommons.org/licenses/by-nd/4.0'
+      }))
+    })
+
+    let err: Error | undefined
+    try {
+      generateReadmeForWorkspace(bundle.books.toArray())
+    } catch (e) {
+      err = e as Error
+    }
+
+    expect(err).toBeDefined()
+    err = err as Error
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string
+    expect(err.toString()).toBe('Error: Licenses differ between collections')
+  })
+
+  it('fails for empty book set', () => {
+    let err: Error | undefined
+    try {
+      generateReadmeForWorkspace([])
+    } catch (e) {
+      err = e as Error
+    }
+    expect(err).toBeDefined()
+    err = err as Error
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string
+    expect(err.toString()).toBe('Error: Got empty book set when generating README')
+  })
+})
---
+++ server/src/readme-generator.ts
@@ -0,0 +1,92 @@
+import { licenseEqual } from './model/cc-license'
+import { expectValue } from './model/utils'
+
+import { BookNode } from './model/book'
+
+// Maybe these templates should be in files so they can be edited more easily?
+const bookTemplate = `\
+# {{ book_title }}
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/from-referrer/)
+
+_{{ book_title }}_ is a textbook published by [OpenStax](https://openstax.org/), a non profit organization that is part of [Rice University](https://www.rice.edu/).
+
+The book can be viewed [online]({{ book_link }}), where you can also see a list of contributors.
+
+## License
+This book is available under the [{{ license_text }}](./LICENSE) license.
+
+## Support
+If you would like to support the creation of free textbooks for students, your [donations are welcome](https://riceconnect.rice.edu/donation/support-openstax-banner).
+`
+
+const bundleTemplate = `\
+# {{ book_titles }}
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/from-referrer/)
+
+_{{ book_titles }}_ are textbooks published by [OpenStax](https://openstax.org/), a non profit organization that is part of [Rice University](https://www.rice.edu/).
+
+To view these books online and view contributors, please visit:
+{{ book_links }}
+
+## License
+These books are available under the [{{ license_text }}](./LICENSE) license.
+
+## Support
+If you would like to support the creation of free textbooks for students, your [donations are welcome](https://riceconnect.rice.edu/donation/support-openstax-banner).
+`
+
+const BOOK_WEB_ROOT = 'https://openstax.org/details/books/'
+
+function populateTemplate(template: string, replacements: Record<string, string>) {
+  // '{{ x }}' -> replacements['x']
+  return template.replace(/\{{2}.+?\}{2}/g, m => {
+    const prop = m.slice(2, -2)
+    const value = expectValue(replacements[prop.trim()], `${prop} is undefined`)
+    return value
+  })
+}
+
+function createBookReadme(book: BookNode) {
+  const license = book.license
+  const replacements = {
+    book_title: book.title,
+    book_link: `${BOOK_WEB_ROOT}${encodeURIComponent(book.slug)}`,
+    license_text: license.text,
+    license_type: license.type,
+    license_version: license.version
+  }
+  return populateTemplate(bookTemplate, replacements)
+}
+
+function createBundleReadme(bundle: BookNode[]) {
+  const firstLicense = bundle[0].license
+  if (!bundle.every(book => licenseEqual(book.license, firstLicense))) {
+    throw new Error('Licenses differ between collections')
+  }
+  const replacements = {
+    book_titles:
+      bundle.map((s, i) =>
+        i === bundle.length - 1 ? `and ${s.title}` : s.title
+      ).join(bundle.length > 2 ? ', ' : ' '),
+    book_links:
+      bundle.map(s =>
+        `- _${s.title}_ [online](${BOOK_WEB_ROOT}${encodeURIComponent(s.slug)})`
+      ).join('\n'),
+    license_text: firstLicense.text,
+    license_type: firstLicense.type,
+    license_version: firstLicense.version
+  }
+  return populateTemplate(bundleTemplate, replacements)
+}
+
+export function generateReadmeForWorkspace(books: BookNode[]) {
+  if (books.length > 1) {
+    return createBundleReadme(books)
+  } else if (books.length > 0) {
+    return createBookReadme(books[0])
+  } else {
+    throw new Error('Got empty book set when generating README')
+  }
+}
