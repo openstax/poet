913390d4120553c03b3e61114b317f9e95efa197
philschatz <253202+philschatz@users.noreply.github.com>
2023-12-12 17:18:39 +0000

    :bug: fix cypress coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: { baseUrl: null }
+  e2e: {
+    baseUrl: null,
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    },
+  }
 })
